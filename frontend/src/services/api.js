27ea5190946cdfcfaa9085bbec008c6cda082557
Kevin23504 <kevinwaghela123@gmail.com>
2026-07-12 13:51:27 +0530

    Fix frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://support-crm-r1z2.onrender.com/api",
 });
 
 export default API;
